031e49e11e90f11e981793f22b4784a6360ab098
Ben Davis <davis.benjamin41902@gmail.com>
2025-10-31 20:19:02 -0700

    drizzle?

--- apps/sv-web/src/routes/+page.server.ts
+++ apps/sv-web/src/routes/+page.server.ts
@@ -2,7 +2,17 @@ import { dbClient } from '$lib/db';
 import { DB_SCHEMA } from '@r8y/db';
 
 export const load = async () => {
-	const result = await dbClient.select().from(DB_SCHEMA.channels);
+	const result = [
+		{
+			name: 'Channel 1'
+		},
+		{
+			name: 'Channel 2'
+		},
+		{
+			name: 'Channel 3'
+		}
+	];
 
 	return {
 		channels: result
