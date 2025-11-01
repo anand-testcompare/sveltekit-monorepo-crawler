16cf5f56953f5ed45ef08d795dfcefdc5f7fb9bc
Ben Davis <davis.benjamin41902@gmail.com>
2025-10-31 19:54:23 -0700

    env vars build fix?

--- apps/sv-web/src/lib/db/index.ts
+++ apps/sv-web/src/lib/db/index.ts
@@ -1,11 +1,11 @@
 import { dev } from '$app/environment';
-import { DATABASE_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { getDbConnection, type DbConnection } from '@r8y/db';
 
 const globalForDb = globalThis as unknown as {
 	client: DbConnection | undefined;
 };
 
-export const dbClient = globalForDb.client ?? getDbConnection(DATABASE_URL);
+export const dbClient = globalForDb.client ?? getDbConnection(env.DATABASE_URL);
 
 if (dev) globalForDb.client = dbClient;
