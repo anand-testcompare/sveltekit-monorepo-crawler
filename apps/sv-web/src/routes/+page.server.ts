381ee0e1ebf11f2a2f4ca307fdd1919333a99c89
Ben Davis <davis.benjamin41902@gmail.com>
2025-10-31 21:15:33 -0700

    PLEASE GOD

--- apps/sv-web/src/routes/+page.server.ts
+++ apps/sv-web/src/routes/+page.server.ts
@@ -1,4 +1,9 @@
+import { dbClient } from '$lib/db';
+import { DB_SCHEMA } from '@r8y/db';
+
 export const load = async () => {
+	const testing = await dbClient.select().from(DB_SCHEMA.channels);
+	console.log(testing);
 	const result = [
 		{
 			name: 'Channel 1'
