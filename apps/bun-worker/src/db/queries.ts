7ec3822b5e95efe0a5c6a6d8b3025f07ba02e72e
Ben Davis <davis.benjamin41902@gmail.com>
2025-10-31 20:34:51 -0700

    testing queries

--- apps/bun-worker/src/db/queries.ts
+++ apps/bun-worker/src/db/queries.ts
@@ -1,1 +1,18 @@
-export const DB_QUERIES = {}
+import { ResultAsync } from 'neverthrow';
+import { dbClient } from '.';
+import { DB_SCHEMA } from '@r8y/db';
+
+export const DB_QUERIES = {
+	getAllChannels: async () => {
+		const result = await ResultAsync.fromPromise(
+			dbClient.select().from(DB_SCHEMA.channels),
+			() => new Error('Failed to get all channels')
+		);
+		return result.match(
+			(channels) => channels,
+			(error) => {
+				throw error;
+			}
+		);
+	}
+};
--- apps/bun-worker/src/index.ts
+++ apps/bun-worker/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello via deej!');
+import { DB_QUERIES } from './db/queries';
+
+const main = async () => {
+	const channels = await DB_QUERIES.getAllChannels();
+	console.log(channels);
+};
+
+main().catch(console.error);
