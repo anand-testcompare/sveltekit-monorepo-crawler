a157295dbf5fa7445d35a4229065de2da1ce09bc
Ben Davis <davis.benjamin41902@gmail.com>
2025-10-31 20:02:03 -0700

    R I P

--- apps/sv-web/src/routes/+page.svelte
+++ apps/sv-web/src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
 	import logo from '$lib/assets/favicon.svg';
-	import { remoteDemo } from '$lib/demo.remote';
-
-	const { channels } = await remoteDemo();
 </script>
 
 <main class="flex grow flex-col items-center justify-center gap-4">
@@ -11,9 +8,9 @@
 		<h2 class="text-3xl font-bold"><span class="text-primary">SvelteKit</span> App</h2>
 	</div>
 	<p class="text-center text-lg text-neutral-500">Welcome to your new SvelteKit project.</p>
-	{#each channels as channel}
+	<!-- {#each channels as channel}
 		<div class="flex flex-row items-center justify-center gap-2">
 			<p>{channel.name}</p>
 		</div>
-	{/each}
+	{/each} -->
 </main>
